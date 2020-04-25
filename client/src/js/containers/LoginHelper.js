49d775d7d0ba39741421b63f87e979ed62685f67
Nimo Beeren <nimo@deity.io>
2020-04-25 16:29:24 +0200

    fix componentWillMount deprecation

--- client/src/js/containers/LoginHelper.js
+++ client/src/js/containers/LoginHelper.js
@@ -5,7 +5,7 @@ import { authenticate } from "../actions/AuthenticateActions";
 import { anonymousRegister } from "../actions/RegisterActions";
 
 class LoginHelper extends Component {
-  componentWillMount() {
+  componentDidMount() {
     const { currentUser, authenticate, anonymousRegister } = this.props;
     if (currentUser) {
       authenticate(currentUser);
